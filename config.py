7fce2dea53ce066663cb8f8b9454285d04d3c7de
Babast27 <bastien.guillou8@gmail.com>
2025-05-19 10:41:32 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+STREAMLIT_LAYOUT = "wide"
+PROJECTS = {
+    "Analyse crypto": "CryptoAnalyzer",
+    "GARCH":          "GarchModel",
+    "Spot Data":      "SpotPlotter",
+    "Future Data":    "FuturePlotter",
+    "Portefeuille":   "PortfolioTracker"
+}
